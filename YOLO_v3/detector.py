c521dfffa551829c9987c56c7db15f7e155323ba
ycwu <ycwu310@gmail.com>
2018-08-02 18:02:23 +0800

    draft

--- YOLO_v3/detector.py
+++ YOLO_v3/detector.py
@@ -14,6 +14,7 @@ import pickle as pkl
 import pandas as pd
 import random
 
+
 # python detector.py --images 'data/dog-cycle-car.png' --det 'det' --weights 'data/yolov3.weights'
 def arg_parse():
     """
@@ -93,7 +94,7 @@ def load_network(args):
     return model
 
 
-def read_images(args,inp_dim):
+def read_images(args, inp_dim):
     if not os.path.exists(args.det):
         os.makedirs(args.det)
     read_dir = time.time()
@@ -135,7 +136,7 @@ def read_images(args,inp_dim):
                                                batch_size, len(im_batches))]))
             for i in range(num_batches)
         ]
-    return (imlist,im_batches)
+    return (imlist, im_batches, im_dim_list, loaded_ims, read_dir, load_batch)
 
 
 def detection_loop(args, num_classes):
@@ -144,7 +145,8 @@ def detection_loop(args, num_classes):
     CUDA = torch.cuda.is_available()
     model = load_network(args)
     inp_dim = int(model.net_info["height"])
-    imlist,im_batches = read_images(args,inp_dim)
+    imlist, im_batches, im_dim_list, loaded_ims, read_dir, load_batch = read_images(
+        args, inp_dim)
     for i, batch in enumerate(im_batches):
         #load the image
         start = time.time()
@@ -173,16 +175,18 @@ def detection_loop(args, num_classes):
         prediction[:,
                    0] += i * args.bs  #transform the atribute from index in batch to index in imlist
 
-        if not write:                      #If we have't initialised output
+        if not write:  #If we have't initialised output
             output = prediction
             write = 1
         else:
-            output = torch.cat((output,prediction))
+            output = torch.cat((output, prediction))
 
-        for im_num, image in enumerate(imlist[i*args.bs: min((i +  1)*args.bs, len(imlist))]):
-            im_id = i*args.bs + im_num
+        for im_num, image in enumerate(
+                imlist[i * args.bs:min((i + 1) * args.bs, len(imlist))]):
+            im_id = i * args.bs + im_num
             objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
-            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/args.bs))
+            print("{0:20s} predicted in {1:6.3f} seconds".format(
+                image.split("/")[-1], (end - start) / args.bs))
             print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
             print("----------------------------------------------------------")
 
@@ -191,8 +195,72 @@ def detection_loop(args, num_classes):
     try:
         output
     except NameError:
-        print ("No detections were made")
+        print("No detections were made")
         exit()
+
+    im_dim_list = torch.index_select(im_dim_list, 0, output[:, 0].long())
+
+    scaling_factor = torch.min(inp_dim / im_dim_list, 1)[0].view(-1, 1)
+
+    output[:, [1, 3]] -= (
+        inp_dim - scaling_factor * im_dim_list[:, 0].view(-1, 1)) / 2
+    output[:, [2, 4]] -= (
+        inp_dim - scaling_factor * im_dim_list[:, 1].view(-1, 1)) / 2
+
+    output[:, 1:5] /= scaling_factor
+
+    for i in range(output.shape[0]):
+        output[i, [1, 3]] = torch.clamp(output[i, [1, 3]], 0.0,
+                                        im_dim_list[i, 0])
+        output[i, [2, 4]] = torch.clamp(output[i, [2, 4]], 0.0,
+                                        im_dim_list[i, 1])
+
+    class_load = time.time()
+    colors = pkl.load(open("data/pallete", "rb"))
+    draw = time.time()
+
+    def Write(x, results, color):
+        c1 = tuple(x[1:3].int())
+        c2 = tuple(x[3:5].int())
+        img = results[int(x[0])]
+        cls = int(x[-1])
+        label = "{0}".format(classes[cls])
+        cv2.rectangle(img, c1, c2, color, 1)
+        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
+        c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+        cv2.rectangle(img, c1, c2, color, -1)
+        cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4),
+                    cv2.FONT_HERSHEY_PLAIN, 1, [225, 255, 255], 1)
+        return img
+
+    list(map(lambda x: Write(x, loaded_ims,colors[1]), output))
+    det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(args.det,x.split("/")[-1]))
+
+    list(map(cv2.imwrite, det_names, loaded_ims))
+    end = time.time()
+
+    print("SUMMARY")
+    print("----------------------------------------------------------")
+    print("{:25s}: {}".format("Task", "Time Taken (in seconds)"))
+    print()
+    print("{:25s}: {:2.3f}".format("Reading addresses", load_batch - read_dir))
+    print("{:25s}: {:2.3f}".format("Loading batch",
+                                   start_det_loop - load_batch))
+    print("{:25s}: {:2.3f}".format(
+        "Detection (" + str(len(imlist)) + " images)",
+        output_recast - start_det_loop))
+    print("{:25s}: {:2.3f}".format("Output Processing",
+                                   class_load - output_recast))
+    print("{:25s}: {:2.3f}".format("Drawing Boxes", end - draw))
+    print("{:25s}: {:2.3f}".format("Average time_per_img",
+                                   (end - load_batch) / len(imlist)))
+    print("----------------------------------------------------------")
+
+    torch.cuda.empty_cache()
+
+
+
+
 
 
 
@@ -203,7 +271,6 @@ if __name__ == '__main__':
     # confidence = float(args.confidence)
     # nms_thesh = float(args.nms_thresh)
     # start = 0
-
 
     num_classes = 80  #For COCO
     classes = load_classes("data/coco.names")
